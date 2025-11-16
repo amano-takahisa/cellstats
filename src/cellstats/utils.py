8aa68dc29b1bc671208dfa169eba6ba3e50c7f25
Taka <amano.takahisa@gmail.com>
2025-11-16 18:43:54 +0100

    add plot_image

--- src/cellstats/utils.py
+++ src/cellstats/utils.py
@@ -1,8 +1,25 @@
 import math
+from pathlib import Path
+from typing import Literal, TypeAlias
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import skimage
 import skimage.measure
+from matplotlib.colors import LinearSegmentedColormap
+from skimage.measure import label, regionprops_table
+
+
+def plot_image(image_path: Path, band: int = 0, band_color: str = 'red'):
+    cmap = LinearSegmentedColormap.from_list(
+        'cmap', [(0, 'white'), (1, band_color)]
+    )
+    image_array = skimage.io.imread(image_path)
+    _, axs = plt.subplots(1, 2, figsize=(16, 8))
+    axs[0].matshow(image_array)
+    axs[1].matshow(image_array[:, :, band], cmap=cmap, vmin=0, vmax=255)
+    plt.show()
 
 
 def plot_regions(
